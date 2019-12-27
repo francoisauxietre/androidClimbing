20994795e760497d5408e9f44a5e625a61f5a5bc
f.Auxietre <f.Auxietre@gmail.com>
2019-12-27 02:42:18 +0100

    add Card serializable to cardActivity

--- app/src/main/java/frox/world/com/controller/CardActivity.java
+++ app/src/main/java/frox/world/com/controller/CardActivity.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import frox.world.com.R;
 import frox.world.com.model.Card;
 
 public class CardActivity extends AppCompatActivity {
     private Card card;
+    private TextView nom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,8 @@ public class CardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_card);
         Intent intent = getIntent();
         card = (Card) intent.getSerializableExtra("card");
+        nom = findViewById(R.id.activity_card_textview_name);
+        nom.setText(card.getNom());
 
     }
 }
